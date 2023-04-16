9b63f4bb1ee1961c5b54fc956dd41a3a63f357f7
Lewis Farnworth <lewfar99@gmail.com>
2023-04-16 21:14:27 +0100

    Moved tabnumber to before file name

--- lua/farndog/lualine.lua
+++ lua/farndog/lualine.lua
@@ -20,7 +20,7 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename', 'tabs'},
+    lualine_c = { 'tabs', 'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
