4424149a8057a4f5ee30380e197775e99b6c077a
Lewis Farnworth <lewfar99@gmail.com>
2023-05-02 10:59:14 +0100

    Removed annoyingly pointless prettier config

--- lua/farndog/prettier.lua
+++ lua/farndog/prettier.lua
@@ -17,16 +17,6 @@ prettier.setup({
     "typescriptreact",
     "yaml",
   },
-  ['null-ls'] = {
-      condition = function() 
-          return prettier.config_exists({
-              -- If 'false', skips checking config.json
-              check_package_json = true
-          })
-      end,
-      timeout = 5000
-  },
-  config_precedence = 'prefer-file',
   cli_options = {
     arrowParens = "avoid",
     bracketSpacing = true,
