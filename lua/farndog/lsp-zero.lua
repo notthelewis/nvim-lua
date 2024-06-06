caca984cef9d5baacfb8e314328fec0249aecb01
wsl-11-2023 <lewfar99@gmail.com>
2024-06-06 20:57:19 +0100

    feat(conf): lsp-zero revamp
    - Remove reference to deprecated APIs (`nvim_workspace` & ``)
    - Integrate mason-lspconfig with lsp-zero
    - Move virtual-text conf into theme file

--- lua/farndog/lsp-zero.lua
+++ lua/farndog/lsp-zero.lua
@@ -1,88 +1,118 @@
-local lsp = require('lsp-zero');
+local lsp = require('lsp-zero').preset({
+  -- name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
+})
+
+local lspconfig = require('lspconfig');
 local cmp = require('cmp');
 
-lsp.preset('recommended');
+-- Completion engine setup
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
 
-lsp.ensure_installed({
-	-- 'tsserver',
-	-- 'eslint',
-	'lua_ls',
-	'rust_analyzer',
-    'emmet_ls'
+            -- potential replacement in future
+            -- Thinking of adding support to switch into the window with <C-w> and do searching/filtering in 
+            -- completions/documentation.. Big project but could be fun 
+            --
+            -- vim.snippet.expand(args.body)
+        end,
+    },
+    window = {
+        -- completion = cmp.config.window.bordered();
+        -- documentation = cmp.config.window.bordered();
+        documentation = {
+            max_height = 15,
+            max_width = 60,
+            border = 'rounded',
+            col_offset = 0,
+            side_padding = 1,
+            winhighlight = 'Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None',
+            zindex = 1001
+        },
+    },
+    -- view = {
+    --     entries = "native"
+    -- },
+    mapping = cmp.mapping.preset.insert({
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<C-u>'] = cmp.mapping.scroll_docs(4) ,
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4) ,
+        ['<CR>'] = cmp.mapping.confirm({ select = true })
+    }),
+    sources = cmp.config.sources({
+        {name = 'nvim_lsp'},
+        {name = 'nvim_lua'},
+        {name = 'buffer'},
+        {name = 'luasnip'},
+        {name = 'path'},
+    }),
 });
 
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<S-Tab>'] = cmp.mapping.select_prev_item(),
-	['<Tab>'] = cmp.mapping.select_next_item(),
-	['<CR>'] = cmp.mapping.confirm({ select = true })
+-- Specific LSP setup 
+local capabilities = require('cmp_nvim_lsp').default_capabilities();
+require('mason').setup({});
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        'tsserver',
+        'rust_analyzer',
+        'lua_ls',
+        'emmet_ls',
+        'gopls',
+    },
+    handlers = {
+        function(server_name)
+            lspconfig[server_name].setup({
+                capabilities = capabilities,
+            })
+        end,
+
+        lua_ls = function()
+            lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+        end,
+
+        emmet_ls = function()
+            lspconfig.emmet_ls.setup({
+                filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue", "hbs" },
+                init_options = {
+                    html = {
+                        options = {
+                            -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+                            ["bem.enabled"] = true,
+                        },
+                    },
+                }
+            })
+        end
+    }
 });
 
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
-});
-
-
-lsp.on_attach(function(client, bufnr)
-	local opts = { buffer = bufnr };
+-- Keybindings
+lsp.on_attach(function (_, bufnr)
+   local opts = { buffer = bufnr };
 
 	-- code, go to definitions
 	vim.keymap.set('n', '<leader>cgd', function() vim.lsp.buf.definition() end, opts);
 	vim.keymap.set('n', '<leader>cgt', function() vim.lsp.buf.type_definition() end, opts);
 	vim.keymap.set('n', '<leader>cgr', function() vim.lsp.buf.references() end, opts);
-	vim.keymap.set('n', '<leader>cga', function() vim.lsp.buf.code_action() end, opts);
 
-    -- go to errors
+	-- code action
+	vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+
+	-- code rename symbol
+	vim.keymap.set('n', '<leader>crn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+
+	-- code show errors
+	vim.keymap.set('n', '<leader>cse', '<cmd>lua vim.diagnostic.open_float(0, { scope = "line", border = "single" })<CR>', opts)
+
+	-- go to errors
 	vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, opts);
 	vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts);
 
-	-- code rename symbol
-    vim.keymap.set('n', '<leader>crn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-
-    -- code show errors
-    vim.keymap.set('n', '<leader>cse', '<cmd>lua vim.diagnostic.open_float(0, { scope = "line", border = "single" })<CR>', opts)
-
-    -- Code action
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-
-    -- function signature help
+	-- function signature help
 	vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts);
-end);
-
-lsp.nvim_workspace()
-lsp.setup();
-
-lsp.setup({
-    ["emmet_ls"] = {
-        filetypes = {
-             "css",
-             "eruby",
-             "html",
-             "javascript",
-             "javascriptreact",
-             "less",
-             "sass",
-             "scss",
-             "svelte",
-             "pug",
-             "typescriptreact",
-             "vue",
-             "hbs"
-         }
-    }
-})
-
--- lsp.emmet_ls.setup({
---     filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue", "hbs" },
---     init_options = {
---       html = {
---         options = {
---           -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
---           ["bem.enabled"] = true,
---         },
---       },
---     }
--- })
-
-vim.diagnostic.config({
-    virtual_text = true
-})
+end)
